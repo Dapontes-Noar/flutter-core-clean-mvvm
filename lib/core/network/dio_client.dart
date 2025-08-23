055026637a6aab6b384c9e21de863cced92f83f4
dapontes-noar <dianaa2054@gmail.com>
2025-08-22 23:38:35 -0500

    Add Dio integration and implement register module for dependency injection

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,74 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_core_clean_mvvm/core/network/exceptions/dio_client_exception.dart';
+import 'package:injectable/injectable.dart';
+import 'api_constants.dart';
+import 'interceptors/logger_interceptor.dart';
+
+@lazySingleton
+class DioClient {
+  final Dio _dio;
+
+  DioClient(this._dio, {List<Interceptor>? interceptors}) {
+    if (!_dio.interceptors.any((i) => i is LoggerInterceptor)) {
+      _dio.interceptors.add(LoggerInterceptor(dio: _dio));
+    }
+    if (interceptors != null) {
+      _dio.interceptors.addAll(interceptors);
+    }
+  }
+
+  Dio get instance => _dio;
+
+  void setHeaders(Map<String, dynamic> headers) {
+    _dio.options.headers.addAll(headers);
+  }
+
+  Future<T> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    required T Function(dynamic data) fromJson,
+    Options? options,
+  }) async {
+    try {
+      final response = await _dio.get(
+        path,
+        queryParameters: queryParameters,
+        options: options,
+      );
+      return fromJson(response.data);
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<T> post<T>(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    required T Function(dynamic data) fromJson,
+    Options? options,
+  }) async {
+    try {
+      final response = await _dio.post(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+      );
+      return fromJson(response.data);
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Exception _handleError(dynamic error) {
+    if (error is DioException) {
+      return DioClientException(
+        error.message ?? 'Unknown Dio error',
+        statusCode: error.response?.statusCode,
+      );
+    } else {
+      return DioClientException('Unexpected error: $error');
+    }
+  }
+}
